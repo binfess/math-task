5571393820a7e9916583ff603d9ffe6365d19476
binfess <binfess@yandex.ru>
2019-08-25 23:08:43 +0300

    add base version CDArray and generation 700 numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include <random>
 
-int main(int argv, char **argv)
+#include "cdarray.hpp"
+
+int main(int argc, char **argv)
 {
+	CDArray<int> numbers(700);
 
+	int max = 50, min = 10;
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<> dis(min, max);
+
+	for (int i = 0; i < 700; i++)
+	{
+		numbers.Add(dis(gen));
+		std::cout << numbers[i] << std::endl;
+	}
 
 	return 0;
 }
